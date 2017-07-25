538e4c1686db58d3188908c4564eb980552e4ccd
Juan Paclan <paclan9@hotmail.com>
2017-07-25 15:34:47 -0500

    practica de traces

--- traces.js
+++ traces.js
@@ -1,26 +1,36 @@
 var domain = require("domain");
 var strackUp = require("stackup");
 var readFile = require("fs").readFile;
-
 module.exports = scenario;
 function scenario(jsonPath, cb) {
-
-function doSomethingDangerousAndAsync(cb) {
      // create a new domain
      var d = domain.create();
-
      // "handle" the error (OMG DON'T DO THIS IN PRODUCTION CODE)
-     d.on("error", function(err){
-       console.log("domain1 handled this error ("+err.message+")");
-     });
-
+     d.on("error", cb);
      // use the domain
      d.run(function () {
-
          readFile(jsonPath, {encoding : "utf8"}, function (error, contents) {
            cb(error, JSON.parse(contents));
          });
-       doDangerousAsyncThing(cb);
      });
    }
-   }
+// solucion 
+// require("stackup");
+//
+//   var domain = require("domain");
+//   var readFile = require("fs").readFile;
+//
+//   module.exports = scenario;
+//
+//   function scenario(jsonPath, cb) {
+//     var d = domain.create();
+//
+//     d.on("error", cb);
+//
+//     d.run(function () {
+//       readFile(jsonPath, {encoding : "utf8"}, function (error, contents) {
+//         cb(error, JSON.parse(contents));
+//       });
+//     });
+//   }
+//
